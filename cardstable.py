86371f4ec324b2d672f6b05b960bfb40cae73ae0
eladj <eladjoseph@hotmail.com>
2013-09-30 23:53:47 +0300

    Major change
    Change cards naming convention
    add class CardItem (not the old one)
    change the class structure

--- cardstable.py
+++ cardstable.py
@@ -10,14 +10,24 @@ class cardTableWidget(QWidget):
     def __init__(self, parent=None):
         super(QWidget, self).__init__(parent)       
         self.scene = QGraphicsScene()
-        self.scene.setSceneRect(0, 0, 640, 480)
+        #self.scene.setSceneRect(0, 0, 640, 480)
         self.view = QGraphicsView(self.scene)
         self.view.setRenderHint(QPainter.Antialiasing)        
         layout = QHBoxLayout()
         layout.addWidget(self.view)
         self.setLayout(layout)
         self.setBackgroundColor(QColor('green'))     
-        self.cardsList = [] #holds all the cards items
+        # special properties
+        self.svgCardsPath = "svg"
+        self.cardsList = [] #holds all the cards names
+        self.cardsGraphItems = [] #holds all the cards items
+        self.defInsertionPos = QPointF(0,0)
+        self.defAngle = 0
+        self.defScale = 0.7
+        self.deckBackSVG = 'back_1'
+        self.numOfPlayers = 4
+        self.playersHandsPos = [(-300,150,90),(-100,0,180),(300,100,270),(-100,300,0)] #(x,y,angle)
+        self.defHandSpacing = 24
         #self.test1()
 
 
@@ -28,106 +38,114 @@ class cardTableWidget(QWidget):
         self.scene.backgroundBrush() 
 
 
-    def cardSvgFile(self, suit, rank):
-        path = "svg"        
-        fn = os.path.join(path,suit + "_" + rank + ".svg")        
+    def cardSvgFile(self, name):
+        """ get card svg file from card name 
+        name = 'c_4','d_Q',...
+        for jokers name = 'j_r' or 'j_b'
+        for back name = 'back_1', 'back_2', ...
+        """
+        fn = os.path.join(self.svgCardsPath,name + ".svg")        
         return fn
 
+            
+    def addCard(self, name, faceDown=False, player=0):
+        """ adds CardGraphicsItem graphics to board.
+        also updates the total cards list
+        """        
+        # svg file of the card graphics
+        if faceDown:
+            svgFile = self.cardSvgFile(self.deckBackSVG)            
+        else:
+            svgFile = self.cardSvgFile(name)
         
-    # adds CardItem to board    
-    def addCard(self, CardItem):
-        self.cardsList.append(CardItem)
-        self.scene.addItem(self.cardsList[-1])        
+        self.cardsList.append(CardItem(name,self.value(name),player,faceDown))
+        ind = len(self.cardsGraphItems) + 1
+        position = self.defInsertionPos
+        angle = self.defAngle        
+        scale = self.defScale
+        tmp = CardGraphicsItem(name, ind, position, svgFile, angle, scale)
+        tmp.setZValue(ind) # set ZValue as index (last in is up)
+        self.cardsGraphItems.append(tmp)
+        self.scene.addItem(self.cardsGraphItems[-1])
+        
+        # sanity check
+        self.checkLists()
+
         #print("num of cards=" + str(len(self.cardsList)))
 
-
-    # removes card from board    
     def removeCard(self, cardIndex):
-        self.scene.removeItem(self.cardsList[cardIndex])
+        """ removes CardGraphicsItem graphics from board 
+        also removes from the total cards list
+        """
+        self.scene.removeItem(self.cardsGraphItems[cardIndex])
+        self.cardsGraphItems.pop(cardIndex)        
+        self.cardsList.pop(cardIndex)
+        self.checkLists()
         #print("num of cards=" + str(len(self.cardsList)))
 
 
-    # replaces CardItem
-    def changeCard(self, cardItemRemove, cardItemAdd):       
-        cardItemAdd.setZValue(cardItemRemove.zValue())
-        self.scene.removeItem(cardItemRemove)
-        self.scene.addItem(cardItemAdd)
+    # replaces CardGraphicsItem
+    def changeCard(self, cardIndRemove, nameToAdd, faceDown=False):       
+        """ replace CardGraphicsItem         
+        keeps same index and ZValue !
+        """
+        
+        zValueTmp = self.cardsGraphItems[cardIndRemove].zValue()
+        position = self.cardsGraphItems[cardIndRemove].pos()
+        angle = self.cardsGraphItems[cardIndRemove].rotation()
+        scale = self.cardsGraphItems[cardIndRemove].scale()
+        self.scene.removeItem(self.cardsGraphItems[cardIndRemove])
+        self.cardsGraphItems.pop(cardIndRemove)
+        player = self.cardsList[cardIndRemove].player
+        self.cardsList.pop(cardIndRemove)
 
-       
+        # svg file of the card graphics
+        if faceDown:
+            svgFile = self.cardSvgFile(self.deckBackSVG)            
+        else:
+            svgFile = self.cardSvgFile(nameToAdd)
+
+        ind = cardIndRemove
+        self.cardsList.insert(ind,CardItem(nameToAdd,self.value(nameToAdd),player,faceDown))        
+        tmp = CardGraphicsItem(nameToAdd, ind, position, svgFile, angle, scale)
+        tmp.setZValue(zValueTmp) # set ZValue as previous
+        self.cardsGraphItems.insert(ind, tmp)
+        self.scene.addItem(self.cardsGraphItems[ind])
+        self.checkLists()
+
+
+    def checkLists(self):
+        """ sanity check """
+        if len(self.cardsList) != len(self.cardsGraphItems):
+            print("WARNING !!! -> cardsList != cardsGraphItems")
+
 
     def getCardsList(self):
-        print("Cards List:")        
-        for item in self.scene.items():
-            try:
-                print("Value= %3d, Name=%s" % (item.getValue(),item.getName()))
-            except:
-                pass
+        """ prints cards list """
+        print("Cards List:")
+        n=1
+        for item in self.cardsList:
+            print("Ind=%3d | Name=%4s | Value= %3d | Player=%d | faceDown=%r " % \
+                 (n, item.name, item.value, item.player, item.faceDown) )
+            n += 1
 
 
-    def sortByRankAndValue(self):
-        pass
-        
-
-    def buildCardDict(self,with_joker=False):
-        suits = ['club','diamond','heart','spade']
-        ranks = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-        n = 1
-        d = dict()
-        for suit in suits:
-            for rank in ranks:
-                d[n] = (suit,rank)
-                n+=1
-        if with_joker:
-            d[n] = ('black','joker')
-            n+=1 
-            d[n] = ('red','joker')
-        return d
-
+    def suit(self, name):
+        """ get card suit type """    
+        return name.split("_")[0]
     
-    def dealDeck(self, numOfPlayes, numOfCardsToEach):
-        random.randrange([1,52])
-
-        
-    def test1(self):
-        c = CardItem('club','3',(50,5),-20)
-        self.scene.addItem(c)
-        c2 = CardItem('heart','Q',(200,100),100,1.5)
-        self.scene.addItem(c2)
-        
-        
-    def test2(self):
-        types = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
-        x=100
-        for name in types:
-            self.scene.addItem(CardItem('club',name,(x,10),scale=0.7))
-            x += 40
-        #self.view.show()
     
-
-class CardItem(QtSvg.QGraphicsSvgItem):
-    def __init__(self, suit, rank, position, angle=0, scale=1, svgFile=None):
-        super(CardItem, self).__init__(svgFile)        
-        self.setPos(position[0],position[1])
-        self.rotate(angle)
-        self.setScale(scale)        
-        self.suit = suit
-        self.rank = rank
-        self.pos = position
-        self.angle = angle
-        self.scale = scale
-        #self.faceDown = faceDown   # set if card is showen or the back
-        self.setAcceptHoverEvents(True) #by Qt default it is set to False
-        #self.setFlag(QGraphicsItem.ItemIsMovable)      
-
+    def rank(self, name):    
+        """ get card rank type """        
+        return name.split("_")[1]
         
-       
     
-    # return card value in number, by their rank
-    def getValue(self):
-        rank = self.rank
-        if rank == 'joker':
-            value = 99
-        elif rank == 'A':
+    def value(self, name):        
+        """ return card value in number, by their rank """
+        if self.suit(name)=='j':
+            return 99
+        rank = self.rank(name)
+        if rank == 'A':
             value = 14
         elif rank == 'K':
             value = 13
@@ -138,37 +156,127 @@ class CardItem(QtSvg.QGraphicsSvgItem):
         else:
             value = int(rank)        
         return value
-            
+        
 
-    def getName(self):
-        return self.suit + "_" + self.rank
+    def buildDeckList(self,with_joker=False):
+        suits = ['c','d','h','s']
+        ranks = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+        l = list()
+        for suit in suits:
+            for rank in ranks:
+                l.append(suit + '_' + rank)
+        if with_joker:
+            l.append('j_b')
+            l.append('j_r')            
+        return l
 
+    
+    def dealDeck(self):
+        d = self.buildDeckList()        
+        random.shuffle(d)
+        print(d)
+        playerNum=1
+        n=1
+        c2=0
+        dx = [0,self.defHandSpacing,0,self.defHandSpacing]
+        dy = [self.defHandSpacing,0,self.defHandSpacing,0]        
+        x, y, ang = self.playersHandsPos[playerNum-1]
+        for card in d:            
+            self.addCard(card,player=playerNum)
+            self.cardsGraphItems[-1].setPos(x+dx[playerNum-1]*c2,
+                                            y+dy[playerNum-1]*c2)
+            self.cardsGraphItems[-1].rotate(ang)
+            if n % (52 / self.numOfPlayers) == 0:
+                playerNum += 1                
+                if playerNum > self.numOfPlayers:
+                    break
+                x, y, ang = self.playersHandsPos[playerNum-1]
+                c2=0
+            n += 1
+            c2 += 1
 
-    # FUTURE: change the card type
-    def changeCardType(self, suit, rank):
-        pass
+        
+    def test1(self):
+        c = CardGraphicsItem('club','3',(50,5),-20)
+        self.scene.addItem(c)
+        c2 = CardGraphicsItem('heart','Q',(200,100),100,1.5)
+        self.scene.addItem(c2)
+        
+        
+    def test2(self):
+        types = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+        x=100
+        for name in types:
+            self.scene.addItem(CardGraphicsItem('club',name,(x,10),scale=0.7))
+            x += 40
+        #self.view.show()
+    
 
-    # when mouse enter a card
+class CardGraphicsItem(QtSvg.QGraphicsSvgItem):
+    """ Extends QtSvg.QGraphicsSvgItem for card items graphics """ 
+    def __init__(self, name, ind, position, svgFile, angle=0, scale=1):
+        super(CardGraphicsItem, self).__init__(svgFile)
+        # special properties
+        self.name = name        
+        self.ind = ind
+        self.svgFile = svgFile
+        # default QGraphicsSvgItem properties
+        self.setPos(position)
+        self.rotate(angle)
+        self.setScale(scale)        
+        self.setAcceptHoverEvents(True) #by Qt default it is set to False
+        #self.setFlag(QGraphicsItem.ItemIsMovable)              
+       
+    
+    def getSuit(self):
+        """ get card suit type """    
+        return self.name.split("_")[0]
+    
+    
+    def getRank(self):    
+        """ get card rank type """        
+        return self.name.split("_")[1]
+        
+    
+    def getValue(self):        
+        """ return card value in number, by their rank """
+        if self.getSuit()=='j':
+            return 99
+        rank = self.getRank()
+        if rank == 'A':
+            value = 14
+        elif rank == 'K':
+            value = 13
+        elif rank == 'Q':
+            value = 12            
+        elif rank == 'J':
+            value = 11
+        else:
+            value = int(rank)        
+        return value            
+
+    
     def hoverEnterEvent(self, event):
+        """ event when mouse enter a card """    
         effect = QGraphicsDropShadowEffect(self)
         effect.setBlurRadius(15)
         effect.setColor(Qt.red)        
         effect.setOffset(QPointF(-5,0))
         self.setGraphicsEffect(effect)        
     
-    
-    # when mouse leaves a card    
-    def hoverLeaveEvent(self, event):    
+      
+    def hoverLeaveEvent(self, event):
+        """ event when mouse leave a card """    
         self.setGraphicsEffect(None) 
 
 
-    # move card to QPointF(x,y), without animation
-    def moveCardToPos(self, QPointF):
+    def moveToPos(self, QPointF):
+        """ move card to QPointF(x,y), without animation """    
         self.setPos(QPointF)
 
 
-    # animate card to QPointF(x,y), with animation
     def animateCardToPos(self, QPointF):
+        """ animate card to QPointF(x,y), with animation """    
         print('Animating..')               
         anim = QPropertyAnimation(self, 'pos')        
         anim.setDuration(150)
@@ -179,6 +287,14 @@ class CardItem(QtSvg.QGraphicsSvgItem):
         anim.finished() #FUTURE - fix error message
         print('Finish animating..')
 
+class CardItem(object):
+    """ holds cards item logical details (no graphics here) """
+    def __init__(self, name, value, player=0, faceDown=False):
+        self.name = name
+        self.value = value
+        self.player = player
+        self.faceDown = faceDown        
+        
         
 def main():
     app = QApplication(sys.argv)
--- main.py
+++ main.py
@@ -5,71 +5,13 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui  import *
 import cardstable
 import random
-
-# extending CardItem
-class CardItemExtend(cardstable.CardItem):
-    #def __init__(self, suit, rank, position, angle=0, scale=(1,1), svgFile=None):
-        #super(CardItemExtend, self).__init__(suit, rank, position, angle, scale, svgFile=None)
-        #self.setAcceptHoverEvents(True) #by Qt default it is set to False
-        #self.setFlag(QGraphicsItem.ItemIsSelectable)
-        #self.setFlag(QGraphicsItem.ItemIsMovable)
-        
-        
-    # when mouse enter a card
-    def hoverEnterEvent(self, event):
-        effect = QGraphicsDropShadowEffect(self)
-        effect.setBlurRadius(15)
-        effect.setColor(Qt.blue)        
-        effect.setOffset(QPointF(-5,0))
-        self.setGraphicsEffect(effect)        
-    
-        
-    def mousePressEvent(self, event):
-        #self.update()
-        print(self.getName())
-        #print(event.pos().x())
-        #print(event.pos().y())
-        #print(self.isSelected())
-        self.animateCardToPos(QPointF(300,300))        
+      
         
 class cardTableWidgetExtend(cardstable.cardTableWidget):
     def __init__(self):
         super(cardTableWidgetExtend, self).__init__()
- 
-    # flips card faceDown
-    def flipCard(self, cardItem, face='down'):
-        if face=='down':
-            tmp = CardItemExtend(cardItem.suit,cardItem.rank,cardItem.pos, 
-                                 cardItem.angle, cardItem.scale,
-                                 svgFile=self.cardSvgFile('back','1'))
-        else:
-            tmp = CardItemExtend(cardItem.suit,cardItem.rank,cardItem.pos, 
-                                 cardItem.angle, cardItem.scale,
-                                 svgFile=self.cardSvgFile(cardItem.suit,cardItem.rank))                                 
-        self.changeCard(cardItem, tmp)       
-        
-    def deal1(self):
-        defScale=0.5        
-        d = self.buildCardDict()
-        inds = range(1,53)
-        random.shuffle(inds)
-        positions = [(100,0),(5,100),(100,300),(300,0)]
-        count = 0
-        positionCounter = 0
-        for n in inds:
-            if count % 13 == 0:
-                pos = positions[positionCounter]
-                positionCounter+=1
-                dx=0
-            suit = d[n][0]
-            rank = d[n][1]
-            c = CardItemExtend(suit,rank,pos,scale=defScale, 
-                                svgFile=self.cardSvgFile(suit,rank))
-            c.setZValue(1)
-            self.addCard(c)
-            dx+=20
-            count += 1
-        
+      
+
     def test2(self):
         types = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
         x=100
@@ -102,14 +44,18 @@ class MainWindow(QMainWindow):
         # set central widget
         self.setCentralWidget(self.centralWidget)
         
-        #self.cardsTable.test2()      
-        #self.cardsTable.changeCard(c2,c4)       
-        #self.cardsTable.flipCard(c3, face='up')
-        #self.cardsTable.removeCard(2)        
-        #print(self.cardsTable.buildCardDict())        
-        #self.cardsTable.scene.addRect(5,-50,140,140,QPen('black'))
+        # PLAYGROUND        
+        self.cardsTable.dealDeck()
+#        self.cardsTable.addCard('c_K')
+#        self.cardsTable.addCard('d_8')
+#        self.cardsTable.cardsGraphItems[1].setPos(234,120)
+#        self.cardsTable.addCard('j_r')
+#        self.cardsTable.cardsGraphItems[2].setPos(234,80)
+#        self.cardsTable.changeCard(1,'h_J', faceDown=True)
+#        self.cardsTable.removeCard(1)
+        
         self.cardsTable.getCardsList()               
-        self.cardsTable.deal1()
+#        self.cardsTable.deal1()
         
         
 if __name__ == "__main__":
